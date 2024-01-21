abe7b0c01dc4268e18ef5d85a1dc277e43073db2
GokulMithran <104688792+GokulMithran@users.noreply.github.com>
2024-01-21 13:50:17 +0530

    Update views.py

--- repo/views.py
+++ repo/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-from django.core.paginator import Paginator
 
-# Create your views here.
-# def home(request):
-#     return render(request,'index.html')
 
 from django.shortcuts import render
 import requests
